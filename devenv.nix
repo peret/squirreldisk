9ba9c5ca0264da0128592a010c91d8465dbf301a
Peter Retzlaff <pe.retzlaff@gmail.com>
2025-09-27 15:30:09 +0200

    Commit devenv files for simple setup of development environment

---
+++ devenv.nix
@@ -0,0 +1,48 @@
+{
+  pkgs,
+  ...
+}:
+{
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    at-spi2-atk
+    atkmm
+    cairo
+    gdk-pixbuf
+    glib
+    gtk3
+    harfbuzz
+    librsvg
+    libsoup_3
+    pango
+    webkitgtk_4_1
+    openssl
+  ];
+
+  # https://devenv.sh/languages/
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+  };
+
+  languages.javascript = {
+    enable = true;
+    npm.enable = true;
+  };
+
+  enterShell = "export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS";
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
